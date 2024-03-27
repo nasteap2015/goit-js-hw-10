fc14e19388f8d0cca25d138b59bd5092a585851f
nasteap2015 <149483716+nasteap2015@users.noreply.github.com>
2024-03-27 22:17:48 +0100

    structure+timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+let userSelectedDate = 0;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    console.log(userSelectedDate);
+  },
+};
+const calendar = flatpickr('#datetime-picker', options);
+
+if (userSelectedDate < Date.now) {
+  window.alert(`Please choose a date in the future`);
+}
